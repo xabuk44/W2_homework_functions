ef0ceb3aae46e47ad412a0cf0c9d7ab3fe74e0d4
xabuk44 <xabuk44@gmail.com>
2019-02-04 16:06:45 +0300

    add Doctest to functions.

--- W2_homework_func_Wallpaper.py
+++ W2_homework_func_Wallpaper.py
@@ -1,5 +1,9 @@
 
 def roll_quantity (wall_length, wall_width, wall_height, wallpaper_length, wallpaper_width):
+    """
+    >>> roll_quantity(5, 6, 2.75, 10, 1.06)
+    8
+    """
     wall_height += 0.1
     room_perimetr = (wall_length + wall_width) * 2
     cloth_quantity = int(round(room_perimetr / wallpaper_width))
--- W2_homework_functions.py
+++ W2_homework_functions.py
@@ -1,5 +1,9 @@
 def fuel_range (value, consumption_per_100km):
+    """
+    >>> fuel_range (50, 10)
+    480.0
+    """
     fuel_consumption_1km = consumption_per_100km / 100
     fuel_range_km = (value / fuel_consumption_1km) - 20
     return fuel_range_km
-print(round(fuel_range(47.6, 10.5)))
+print(round(fuel_range (50, 10)))
