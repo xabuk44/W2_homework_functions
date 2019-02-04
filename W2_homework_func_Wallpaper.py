aa1f369f5deccc96fb706dd3af16aab7a4ab58b6
xabuk44 <xabuk44@gmail.com>
2019-02-04 14:58:24 +0300

    finished wallpaper.py, edited fuel.py

---
+++ W2_homework_func_Wallpaper.py
@@ -0,0 +1,9 @@
+
+def roll_quantity (wall_length, wall_width, wall_height, wallpaper_length, wallpaper_width):
+    wall_height += 0.1
+    room_perimetr = (wall_length + wall_width) * 2
+    cloth_quantity = int(round(room_perimetr / wallpaper_width))
+    cloth_in_one_roll = int(wallpaper_length / wall_height)
+    roll_quantity = int(cloth_quantity / cloth_in_one_roll) + 1
+    return roll_quantity
+print ('You will need', roll_quantity(5, 6, 2.75, 10, 1.06), 'wallpaper rolls')
--- W2_homework_functions.py
+++ W2_homework_functions.py
@@ -1,5 +1,5 @@
-def fuel_consumption (value, consumption_per_100km):
+def fuel_range (value, consumption_per_100km):
     fuel_consumption_1km = consumption_per_100km / 100
-    fuel_range = (value / fuel_consumption_1km) - 20
-    return fuel_range
-print(round(fuel_consumption(47.6, 10.5)))
+    fuel_range_km = (value / fuel_consumption_1km) - 20
+    return fuel_range_km
+print(round(fuel_range(47.6, 10.5)))
