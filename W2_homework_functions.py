73d2a2fcbaab0610e2037757d15283abdedb4be3
xabuk44 <xabuk44@gmail.com>
2019-02-04 14:39:48 +0300

    finished fuel_consumption, added wallpapers.py

--- W2_homework_functions.py
+++ W2_homework_functions.py
@@ -1,1 +1,5 @@
-def fuel_consumption (value, consumtion_per_100km):
+def fuel_consumption (value, consumption_per_100km):
+    fuel_consumption_1km = consumption_per_100km / 100
+    fuel_range = (value / fuel_consumption_1km) - 20
+    return fuel_range
+print(round(fuel_consumption(47.6, 10.5)))
